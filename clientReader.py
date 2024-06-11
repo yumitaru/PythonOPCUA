a1379ef89e031da1b4a92da45b18224a86ae7424
Kromczetto <jk302106@student.polsl.pl>
2024-06-11 23:39:50 +0200

    Support UA Expert, breaking infinite loop with CTRL + C

--- clientReader.py
+++ clientReader.py
@@ -5,10 +5,12 @@ url = "opc.tcp://desktop-vics7it:62640/IntegrationObjects/ServerSimulator/"
 nodeId = "ns=2;s=Tag7"
 client = Client(url)
 client.connect()
-
-while True:
-    node = client.get_node(nodeId)
-    value = node.get_value()
-    print(value)
-    time.sleep(10)
+try:
+    while True:
+        node = client.get_node(nodeId)
+        value = node.get_value()
+        print(value)
+        time.sleep(10)
+except KeyboardInterrupt:
+    print("READING END :( SEE YOU SOON")        
 client.disconnect()
--- clientWriter.py
+++ clientWriter.py
@@ -8,10 +8,13 @@ client = Client(url)
 client.connect()
 
 counter = 0
-while True:
-    node = client.get_node(nodeId)
-    value = int(counter)
-    node.set_data_value(value)
-    counter = counter + 1
-    time.sleep(10)
+try:
+    while True:
+        node = client.get_node(nodeId)
+        value = int(counter)
+        node.set_data_value(value)
+        counter = counter + 1
+        time.sleep(10)
+except KeyboardInterrupt:
+    print("WRITING END :( SEE YOU SOON")        
 client.disconnect()
